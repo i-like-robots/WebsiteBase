34164992c8b3372960d0ff2e8d49dc04559eb829
Matt Hinchliffe <matt@maketea.co.uk>
2012-03-01 14:36:08 +0000

    Abstracted mixins Added media object New form layout markup and classes Variables now hyphenated and not camelCase Clearfix now nested mixin Form fields use content box layout Nestable grid JS hide util class

--- assets/js/project.js
+++ assets/js/project.js
@@ -1,53 +1,30 @@
 /**
  * @author Matt Hinchliffe <http://www.maketea.co.uk>
  * @created 12/01/2011
- * @modified 16/01/2012
+ * @modified 22/02/2012
  * @package yourProject
  */
 
 var project = 'yourProject';
 
 // Project namespace and version number
-if (!window[project])
+window[project] = window[project] || { _autoload: [] };
+
+// Replace no-js
+document.body.className = document.body.className.replace(/\bno-js\b/, '');
+
+// Quick wrapper for a missing console
+if ( ! window.console)
 {
-	window[project] = { _autoload: [] };
-}
+	var console = { history: [] };
 
-/**
- * Replace no-js
- */
-var body = document.documentElement;
-body.className = body.className.replace(/\bno-js\b/, '');
-
-/**
- * Quick wrapper for a missing console
- */
-if (!window.console)
-{
-	var console = {};
-	console.history = [];
-
-	console.log = function(i)
+	console.error = console.log = function(i)
 	{
 		console.history.push(i);
 	}
 }
 
-/**
- * Prototypal inheritance operator support
- * Douglas Crockford <http://javascript.crockford.com/prototypal.html>
- */
-if (typeof Object.create !== 'function') {
-	Object.create = function(o) {
-		function F() {}
-		F.prototype = o;
-		return new F();
-	};
-}
-
-/**
- * Execute methods registered within [project]._autoload in reverse order.
- */
+// Execute methods registered within [project]._autoload in reverse order.
 jQuery(function()
 {
 	for (var i = 0; i < window[project]._autoload.length; i++)
